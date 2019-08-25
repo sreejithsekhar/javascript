1c01c25292dbf5cf1f8191b27237aab76d3e20fc
Balakrishnan <sbalakrishnan@PRCINS.NET>
2019-08-24 20:23:18 -0400

    Loop count

--- coding/sherlockValid.js
+++ coding/sherlockValid.js
@@ -8,21 +8,25 @@ function isValid(s) {
         else charCountArray[index] = 1;
     }
     console.log(charCountArray);
-    let pCount = charCountArray[0];
+    let pCount;
     let missmatchFound = false;
     let diff = 0;
-
-    for (let i = 1; i < 26; i++) {
+    let x, y1, y2;
+    for (let i = 0; i < 26; i++) {
         if (charCountArray[i]) {
-            diff = Math.abs(pCount - charCountArray[i]);
-            console.log("char", charCountArray[i])
-            console.log("diff", diff)
-            if (diff > 1) return "NO";
-            else if (diff === 1) {
-                if (!missmatchFound) missmatchFound = true;
-                else return "NO";
-            } 
+            if(!x) { x = charCountArray[i]; continue;}
+            console.log(charCountArray[i])
+            
         }   
     }
     return "YES";
 }
+
+// 2, 3, 3
+// x =2, y 1 or 3, y =3, x = 2 or 3, x = 2, y = 2
+// 3, 2, 3
+// 3, 2, 2
+
+console.log(isValid("aabbbccc"))
+//console.log(isValid("aaabbccc"))
+//console.log(isValid("aaabbbcc"))
